75c2e81b9e302e80835c71f10f6929d34f99e50a
RyanMSutherland <Ryan.Suth01@gmail.com>
2025-01-17 15:23:54 -0800

    Parallel

--- picarx/manuvering.py
+++ picarx/manuvering.py
@@ -12,8 +12,30 @@ def straight_line(px, speed = 70, angle = 0, wait = 3.0):
     px.backward(speed)
     time.sleep(wait)
 
+def parallel_parking_left(px, speed = 70, wait = 0.25):
+    turn_in_value = 15
+    px.set_dir_servo_angle(0)
+    px.forward(speed)
+    time.sleep(wait)
 
+    px.set_dir_servo_angle(turn_in_value)
+    px.backward(speed/2)
+    time.sleep(wait*2.0)
+
+    px.set_dir_servo_angle(turn_in_value)
+    time.sleep(wait*2.0)
+
+    px.set_dir_servo_angle(0)
+    px.forward(speed/2)
+    time.sleep(wait*2.0)
+
+    px.stop()
+
+
+def parallel_parking_right():
+    pass
 
 if __name__ == "__main__":
     px = Picarx()
-    straight_line(px, 90, 20, 2.0)
+    # straight_line(px)
+    parallel_parking_left(px)
