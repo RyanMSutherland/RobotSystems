cc703ba328f1a018b1b167d795c8d5ddda32e181
RyanMSutherland <Ryan.Suth01@gmail.com>
2025-01-17 13:53:27 -0800

    Push forward test

--- example/1.move.py
+++ example/1.move.py
@@ -10,39 +10,39 @@ if __name__ == "__main__":
         px = Picarx()
 
         # test motor
-        px.forward(30)
+        px.forward(70)
         time.sleep(0.5)
         # test direction servo
         for angle in range(0, 35):
             px.set_dir_servo_angle(angle)
-            time.sleep(0.01)
+            time.sleep(0.1)
         for angle in range(35, -35, -1):
             px.set_dir_servo_angle(angle)
-            time.sleep(0.01)
+            time.sleep(0.1)
         for angle in range(-35, 0):
             px.set_dir_servo_angle(angle)
-            time.sleep(0.01)
+            time.sleep(0.1)
         px.stop()
         time.sleep(1)
         # test cam servos
         for angle in range(0, 35):
             px.set_cam_pan_angle(angle)
-            time.sleep(0.01)
+            time.sleep(0.1)
         for angle in range(35, -35, -1):
             px.set_cam_pan_angle(angle)
-            time.sleep(0.01)        
+            time.sleep(0.1)        
         for angle in range(-35, 0):
             px.set_cam_pan_angle(angle)
-            time.sleep(0.01)
+            time.sleep(0.1)
         for angle in range(0, 35):
             px.set_cam_tilt_angle(angle)
-            time.sleep(0.01)
+            time.sleep(0.1)
         for angle in range(35, -35,-1):
             px.set_cam_tilt_angle(angle)
-            time.sleep(0.01)        
+            time.sleep(0.1)        
         for angle in range(-35, 0):
             px.set_cam_tilt_angle(angle)
-            time.sleep(0.01)
+            time.sleep(0.1)
     finally:
         px.stop()
         time.sleep(0.2)
