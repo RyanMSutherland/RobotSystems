a38b2878e0a4c6b7279c83228795771d5b8d2839
RyanMSutherland <Ryan.Suth01@gmail.com>
2025-01-17 16:02:36 -0800

    K-Turn

--- picarx/manuvering.py
+++ picarx/manuvering.py
@@ -12,30 +12,52 @@ def straight_line(px, speed = 70, angle = 0, wait = 3.0):
     px.backward(speed)
     time.sleep(wait)
 
-def parallel_parking_left(px, speed = 70, wait = 0.25):
+def parallel_parking(px, speed = 70, wait = 1.5, left = False):
     turn_in_value = 15
+    if left:
+        turn_in_value *= -1
     px.set_dir_servo_angle(0)
     px.forward(speed)
     time.sleep(wait)
 
     px.set_dir_servo_angle(turn_in_value)
     px.backward(speed/2)
-    time.sleep(wait*2.0)
+    time.sleep(wait*1.5)
 
-    px.set_dir_servo_angle(turn_in_value)
-    time.sleep(wait*2.0)
+    px.set_dir_servo_angle(-turn_in_value)
+    time.sleep(wait*1.5)
 
     px.set_dir_servo_angle(0)
     px.forward(speed/2)
-    time.sleep(wait*2.0)
+    time.sleep(wait*1.5)
 
     px.stop()
 
+def k_turn(px, speed = 70, wait = 1.5, left = False):
+    turn_in_value = 15
+    if left: 
+        turn_in_value *= -1
 
-def parallel_parking_right():
-    pass
+    px.setdir_servo_angle(turn_in_value)
+    px.forward(speed)
+    time.sleep(wait)
+
+    px.set_dir_servo_angle(-turn_in_value)
+    px.backward(speed)
+    time.sleep(wait)
+
+    px.set_dir_servo_angle(0)
+    px.forward(speed)
+    time.sleep(wait)
+
+    px.stop()
 
 if __name__ == "__main__":
     px = Picarx()
-    # straight_line(px)
-    parallel_parking_left(px)
+    
+    user_value = 0
+    while user_value != 4:
+        input("Please choose from the following options. 1 : Drive Forwawrd/Backwards. 2 : Parallel Parking. 3 : K-Turn. 4 : Quit")
+        # straight_line(px)
+        parallel_parking(px, False)
+        # k_turn(px, False)
