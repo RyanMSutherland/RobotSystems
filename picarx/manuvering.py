d77386ebdde59da52df0fae235e630424277128e
Ryan Sutherland <Ryan.Suth01@gmail.com>
2025-01-23 12:17:33 -0800

    Finalized Manuvering code with debugs

--- picarx/manuvering.py
+++ picarx/manuvering.py
@@ -1,7 +1,13 @@
 from picarx_improved import Picarx
 import time
+import logging
+
+logging_format = "%(asctime)s: %(message)s"
+logging.basicConfig(format=logging_format, level = logging.INFO, datefmt="%H:%M:%S")
+logging.getLogger().setLevel(logging.DEBUG)
 
 def straight_line(px, speed = 70, angle = 0, wait = 3.0):
+    logging.debug(f'Setting angle to {angle} at speed {speed} for {wait} seconds')
     px.set_dir_servo_angle(angle)
     px.forward(speed)
     time.sleep(wait)
@@ -9,30 +15,39 @@ def straight_line(px, speed = 70, angle = 0, wait = 3.0):
     px.stop()
     time.sleep(wait)
 
+    logging.debug(f'Setting angle to {angle} at speed {speed} for {wait} seconds')
+    px.set_dir_servo_angle(-angle)
     px.backward(speed)
     time.sleep(wait)
 
+    logging.debug("Stopping")
     px.stop()
 
 def parallel_parking(px, speed = 70, wait = 1.5, left = False):
     turn_in_value = 15
     if left:
         turn_in_value *= -1
+
+    logging.debug(f'Setting angle to {0} at speed {speed} for {wait} seconds')
     px.set_dir_servo_angle(0)
     px.forward(speed)
     time.sleep(wait)
 
+    logging.debug(f'Setting angle to {turn_in_value} at speed {speed/2} for {wait*1.5} seconds')
     px.set_dir_servo_angle(turn_in_value)
     px.backward(speed/2)
     time.sleep(wait*1.5)
 
+    logging.debug(f'Setting angle to {-turn_in_value} at speed {speed} for {wait*1.5} seconds')
     px.set_dir_servo_angle(-turn_in_value)
     time.sleep(wait*1.5)
 
+    logging.debug(f'Setting angle to {0} at speed {speed/2} for {wait*1.5} seconds')
     px.set_dir_servo_angle(0)
     px.forward(speed/2)
     time.sleep(wait*1.5)
 
+    logging.debig("Stopping")
     px.stop()
 
 def k_turn(px, speed = 70, wait = 1.5, left = False):
@@ -40,6 +55,7 @@ def k_turn(px, speed = 70, wait = 1.5, left = False):
     if left: 
         turn_in_value *= -1
 
+    logging.debug(f'Setting angle to {turn_in_value} at speed {speed} for {wait} seconds')
     px.set_dir_servo_angle(turn_in_value)
     px.forward(speed)
     time.sleep(wait)
@@ -47,6 +63,7 @@ def k_turn(px, speed = 70, wait = 1.5, left = False):
     px.stop()
     time.sleep(wait/10)
 
+    logging.debug(f'Setting angle to {-turn_in_value} at speed {speed} for {wait} seconds')
     px.set_dir_servo_angle(-turn_in_value)
     px.backward(speed)
     time.sleep(wait)
@@ -54,6 +71,7 @@ def k_turn(px, speed = 70, wait = 1.5, left = False):
     px.stop()
     time.sleep(wait/10)
 
+    logging.debug(f'Setting angle to {turn_in_value} at speed {speed} for {wait} seconds')
     px.set_dir_servo_angle(turn_in_value)
     px.forward(speed)
     time.sleep(wait)
@@ -61,10 +79,12 @@ def k_turn(px, speed = 70, wait = 1.5, left = False):
     px.stop()
     time.sleep(wait/10)
 
+    logging.debug(f'Setting angle to {0} at speed {speed} for {wait} seconds')
     px.set_dir_servo_angle(0)
     px.forward(speed)
     time.sleep(wait)
 
+    logging.debug("Stopping")
     px.stop()
 
 if __name__ == "__main__":
