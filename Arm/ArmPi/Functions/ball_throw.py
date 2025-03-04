6fe9559e8b7aa80f8b32656e9951114edffaf581
Ryan Sutherland <Ryan.Suth01@gmail.com>
2025-03-04 12:22:59 -0800

    Save

--- Arm/ArmPi/Functions/ball_throw.py
+++ Arm/ArmPi/Functions/ball_throw.py
@@ -72,7 +72,7 @@ class Ball_Throw(Perception):
                     self.arm_kinematics.setPitchRangeMoving((desired_x, desired_y, self.throw_height), -90, -90, 0, 1000)
                     
                     # Board.setBusServoPulse(self.servo_1_id, self.gripper_closed - self.gripper_open, self.gripper_closed)
-                    # time.sleep(self.sleep_time)
+                    time.sleep(self.sleep_time)
 
                     self.move_home()
 
